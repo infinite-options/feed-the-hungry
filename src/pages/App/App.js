e4012c0ff5cec0ccb3c3f9f2f004560f106ca7d8
htran25 <htran25@ucsc.edu>
2020-06-15 13:38:21 -0700

    merging App.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -9,55 +9,6 @@ import {
   useParams,
 } from "react-router-dom";
 
-// import css
-<<<<<<< HEAD
-import 'bulma/css/bulma.css'; 
-import './App.css';
-import BanksPage from 'pages/Banks/BanksPage';
-import BankPage from 'pages/Bank/BankPage';
-import Header from 'components/Header/Header';
-import Cart from 'pages/Cart/Cart';
-import BankAPI from 'API/BankAPI';
-import CheckoutPage from 'pages/Checkout/CheckoutPage';
-import LoginPage from 'pages/Login/LoginPage';
-import SignupPage from 'pages/Signup/SignupPage';
-
-function App(){
-  // render(){
-    const list = BankAPI.DataLoader();
-    var banks = [];
-    if (list && list.length > 0) {
-      banks = BankAPI.RemoveNull(list);
-      banks = BankAPI.RemoveDuplicatesBy('foodbank_id', banks);
-    }
-    return (
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/login">
-              <LoginPage />
-          </Route>
-          <Route exact path="/signup">
-              <SignupPage />
-          </Route>
-          <Route exact path="/banks">
-              <BanksPage list={banks} />
-          </Route>
-          <Route exact path={"/banks/:bankId/products"}>
-            <BankPage list={banks}/>
-          </Route>
-          <Route exact path={"/banks/:bankId/products/checkout"}>
-            <CheckoutPage list={banks}/>
-          </Route>
-          <Route path="/about">
-          </Route>
-          <Route path="/report">
-          </Route>
-        </Switch>
-      </Router>
-    );
-    // }
-=======
 import "bulma/css/bulma.css";
 import "./App.css";
 // import components
@@ -66,6 +17,8 @@ import BankPage from "pages/Bank/BankPage";
 import Header from "components/Header/Header";
 import BankAPI from "API/BankAPI";
 import CheckoutPage from "pages/Checkout/CheckoutPage";
+import LoginPage from 'pages/Login/LoginPage';
+import SignupPage from 'pages/Signup/SignupPage';
 
 function App() {
   // fetch data
@@ -80,6 +33,12 @@ function App() {
     <Router>
       <Header />
       <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/signup">
+          <SignupPage />
+        </Route>
         <Route exact path="/banks">
           <BanksPage list={banks} />
         </Route>
@@ -94,7 +53,6 @@ function App() {
       </Switch>
     </Router>
   );
->>>>>>> 2cf95ba0c7efb062599aae9235fb202b6a0fe14f
 }
 
 export default App;
