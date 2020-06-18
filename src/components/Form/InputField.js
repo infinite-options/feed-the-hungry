0357852680d24830ef0c3fd89e37c4ba22d76420
htran25 <htran25@ucsc.edu>
2020-06-17 22:08:23 -0700

    InputField now has optional icon parameter

--- src/components/Form/InputField.js
+++ src/components/Form/InputField.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // use this component for all input fields (well, not really all input fields because it still needs modifications)
-const InputField = ({props}) => {
+const InputField = ({props, icon}) => {
   return (
     <div className="field">
         {/* if the input is a checkbox */}
@@ -11,6 +12,11 @@ const InputField = ({props}) => {
           <label className="checkbox" >
             <input {...props} />
             <span className="has-margin-left-0-5">{props.label}</span>
+            {icon && (
+              <span className="icon tooltip">
+                <img src={icon} alt=""></img>
+              </span>
+            )}
           </label>
         </div>
       ) : (
@@ -19,8 +25,13 @@ const InputField = ({props}) => {
           <label>{props.label}</label>
           {/* If input is empty, shows red border and red error msg. 
            To see what attributes 'props' has, go to useField.js */}
-          <div className="control">
-              <input className={props.error.length > 0 ? "input is-danger" : "input"} {...props} />
+          <div className={icon ? "control has-icons-right" : "control"}>
+            <input className={props.error.length > 0 ? "input is-danger" : "input"} {...props} />
+            {icon && (
+              <span className="icon is-right">
+                <FontAwesomeIcon icon={icon} />
+              </span>
+            )}
           </div>
           <p className="help is-danger">{props.error}</p>
         </div>
