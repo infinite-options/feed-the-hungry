7caeea9f8b6fc87e8f2ae90aeb47c3c70fc3125e
tchu1997 <tuong.chu@sjsu.edu>
2020-06-21 15:00:20 -0700

    Updates

---
+++ precache-manifest.ff22b6ee03351331adeb55f8565700d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235b2b81c15279de24ff26d980c28219",
+    "url": "/feed-the-hungry/index.html"
+  },
+  {
+    "revision": "d410c14502defaa8e239",
+    "url": "/feed-the-hungry/static/css/2.65e3e712.chunk.css"
+  },
+  {
+    "revision": "eeb53590df5c78373e3c",
+    "url": "/feed-the-hungry/static/css/main.4b470626.chunk.css"
+  },
+  {
+    "revision": "d410c14502defaa8e239",
+    "url": "/feed-the-hungry/static/js/2.da398f3b.chunk.js"
+  },
+  {
+    "revision": "01ee0d1593b6736a866f17118a5b90e6",
+    "url": "/feed-the-hungry/static/js/2.da398f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb53590df5c78373e3c",
+    "url": "/feed-the-hungry/static/js/main.6b407901.chunk.js"
+  },
+  {
+    "revision": "e81819ce5540c8fb4e02",
+    "url": "/feed-the-hungry/static/js/runtime-main.3b3efc48.js"
+  },
+  {
+    "revision": "af41986fa1e57f1097685a7a78ee5efb",
+    "url": "/feed-the-hungry/static/media/Delivery.af41986f.png"
+  },
+  {
+    "revision": "011b01ba4f6d9b72f2a3ccbb08cbe9dc",
+    "url": "/feed-the-hungry/static/media/Pickup.011b01ba.png"
+  },
+  {
+    "revision": "3c02e81309c434aae887ce7db56f226f",
+    "url": "/feed-the-hungry/static/media/confirmation.3c02e813.svg"
+  },
+  {
+    "revision": "302b7b48f67d89aee5179073649b89b9",
+    "url": "/feed-the-hungry/static/media/empty-cart.302b7b48.svg"
+  },
+  {
+    "revision": "7128b6c528ef07d328ac3cb64aeedb1b",
+    "url": "/feed-the-hungry/static/media/farmers-market.7128b6c5.jpg"
+  },
+  {
+    "revision": "c5a3deab58f454ae53ab6e679192e991",
+    "url": "/feed-the-hungry/static/media/gluten_free.c5a3deab.png"
+  },
+  {
+    "revision": "6694f867be3155ea3882becf265f61ef",
+    "url": "/feed-the-hungry/static/media/halal.6694f867.png"
+  },
+  {
+    "revision": "a05262d351d851812ae86fde789506c2",
+    "url": "/feed-the-hungry/static/media/kosher.a05262d3.png"
+  },
+  {
+    "revision": "b5cece1e46385c9dc2e98824b7688d74",
+    "url": "/feed-the-hungry/static/media/vegan.b5cece1e.png"
+  },
+  {
+    "revision": "b27bed4d8cb556621bd542924fdffbe0",
+    "url": "/feed-the-hungry/static/media/vegetarian.b27bed4d.png"
+  }
+]);
