ccec2e9a5ece242ba91ec5640a37b48c319c4610
htran25 <htran25@ucsc.edu>
2020-06-21 16:59:38 -0700

    fixing bug with determining error message if input is optional

--- src/components/Hooks/useField.js
+++ src/components/Hooks/useField.js
@@ -16,26 +16,42 @@ const useField = (name, type, isRequired=true) => {
     if (event.target.value.length > 0 ) setError('');
   };
 
+  const checkInputs = () => {
+    // case 1: if input is a zip code
+    if (name.toLowerCase() === "zip" && !/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(value)) setError("Invalid zip code");
+    // case 2: if input is entered but we need to verify it given a data
+    // make sure that the data must have method 'contain' (see StateAPI.js, for instance
+    else if (name.toLowerCase() === "state" && value ==="") setError("Invalid state");
+    // case 3: if input is a phone number
+    else if (type === "tel" && !/[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(value)) setError("Invalid phone number");
+    // case 4: if input is an email
+    else if (type === "email" && !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) setError("Invalid email address");
+    // case 5: if input is currency. NOTE: if we use the 'number' input type for non-currency values in the future, this will need to be changed
+    else if (type === "number" && !/^([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$/.test(value)) setError("Invalid currency amount")
+    // case 6: everything looks good!
+    else {
+      return true;
+    }
+    return false; // if anything is setting an error
+  }
+
   // validate inputs if user clicks submit button. Returns true if no errors, false if error detected.
   const validate = () => {
-      // case 1: if input is required but nothing is entered
-      if (isRequired && value.length === 0) setError("This field is required")
-      // case 2: if input is a zip code
-      else if (name.toLowerCase() === "zip" && !/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(value)) setError("Invalid zip code");
-      // case 3: if input is entered but we need to verify it given a data
-      // make sure that the data must have method 'contain' (see StateAPI.js, for instance
-      else if (name.toLowerCase() === "state" && value ==="") setError("Invalid state");
-      // case 4: if input is a phone number
-      else if (type === "tel" && !/[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(value)) setError("Invalid phone number");
-      // case 5: if input is an email
-      else if (type === "email" && !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) setError("Invalid email address");
-      // case 6: if input is currency. NOTE: if we use the 'number' input type for non-currency values in the future, this will need to be changed
-      else if (type === "number" && !/^([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(.[0-9][0-9])?$/.test(value)) setError("Invalid currency amount")
-      else {
-          setError("");
-          return true;
-      }
+    // if input is required and is not filled
+    if (isRequired && value.length === 0) {
+      setError("This field is required");
       return false;
+    }
+    // if input is filled. Checks required & not required inputs since even if 
+    // it's not required we want to validate input if it's filled.
+    else if (value.length > 0) {
+      return checkInputs();
+    }
+    // if input is not required and not filled
+    else {
+      setError("");
+      return true;
+    }
   }
 
   const resetinput = () => {
