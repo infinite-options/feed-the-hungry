fc28e36015f9230b95f2b7e74911265bcec0e5e0
tchu1997 <tuong.chu@sjsu.edu>
2020-07-18 13:08:31 -0700

    fix css of /banks

--- src/pages/Banks/BanksPage.js
+++ src/pages/Banks/BanksPage.js
@@ -7,39 +7,49 @@ import ErrorPage from "pages/Error/ErrorPage";
 import LeafletMap from "components/Map/LeafletMap";
 import Banks from "pages/Banks/Banks";
 import ScrollToTopOnMount from "utils/Scroll/ScrollToTopOnMount";
-import useMarker from 'components/Hooks/useMarker';
+import useMarker from "components/Hooks/useMarker";
 
 function BanksPage() {
-  const url = `https://dc3so1gav1.execute-api.us-west-1.amazonaws.com/dev/api/v2/foodbankinfo`;
-  const { data, isLoading, hasError } = useOurApi(url, {});
-  const marker = useMarker();
+    const url = `https://dc3so1gav1.execute-api.us-west-1.amazonaws.com/dev/api/v2/foodbankinfo`;
+    const { data, isLoading, hasError } = useOurApi(url, {});
+    const marker = useMarker();
 
-  if (isLoading) return <LoadingPage />;
-  if (hasError) return <ErrorPage />;
-  const banks = Object.keys(data).length ? removeDuplicatesByKey('foodbank_id', data.result.result) : [];
-  return (
-    <div className="banks-page-bd">
-      <ScrollToTopOnMount />
-      <div className="columns">
-        <div className="column is-6">
-          <div className="bank-list">
-            <Banks marker={marker} banks={banks} />
-          </div>
-        </div>
-        <div className="column is-6 has-no-padding has-shadow">
-          <div className="sticky">
-            <LeafletMap marker={marker} banks={banks} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    if (isLoading) return <LoadingPage / > ;
+    if (hasError) return <ErrorPage / > ;
+    const banks = Object.keys(data).length ?
+        removeDuplicatesByKey("foodbank_id", data.result.result) :
+        [];
+    return ( <
+        div className = "banks-page-bd" >
+        <
+        ScrollToTopOnMount / > { /* <div className="columns" style={{marginTop:0}}> */ } <
+        div className = "half-left" >
+        <
+        div className = "bank-list" >
+        <
+        Banks marker = { marker }
+        banks = { banks }
+        /> <
+        /div> <
+        /div> <
+        div className = "half-right" >
+        <
+        div className = "sticky" >
+        <
+        LeafletMap marker = { marker }
+        banks = { banks }
+        /> <
+        /div> <
+        /div> { /* </div> */ } <
+        /div>
+    );
 }
+
 const removeDuplicatesByKey = (key, data) => {
-  const obj ={}
-  data.forEach(x => {
-      obj[x[key]] = x;
-  })
-  return Object.values(obj);
-}
+    const obj = {};
+    data.forEach((x) => {
+        obj[x[key]] = x;
+    });
+    return Object.values(obj);
+};
 export default BanksPage;
